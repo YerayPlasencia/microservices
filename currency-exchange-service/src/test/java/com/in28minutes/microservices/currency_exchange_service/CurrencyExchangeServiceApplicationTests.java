1a3f2f04482ca95b9e229e44ac32f5a054c8678a
YerayPlasencia <51329653+YerayPlasencia@users.noreply.github.com>
2020-05-29 12:22:21 +0100

    currency-exchange-service

---
+++ currency-exchange-service/src/test/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.microservices.currency_exchange_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
